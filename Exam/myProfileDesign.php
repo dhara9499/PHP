bc7997ee91d9b0581b4ed89ee7cbc29b2b452b2d
Dhara Parekh <parekhdhara94@gmail.com>
2020-02-03 23:32:27 +0530

    exam

--- Exam/myProfileDesign.php
+++ Exam/myProfileDesign.php
@@ -5,21 +5,24 @@
         <meta charset="UTF-8"></meta>
     </head>
     <?php include_once 'myProfile.php';
-          include_once 'validations.php'; ?>
+          include_once 'validations.php'; 
+          $registrationArray = fetchData('userTable');?>
     <body>
         <form method="POST" name="registrationForm">
             <label for="registration[prefix]">Prefix</label>
                 <select name="registration[prefix]">
                     <?php $prefix = ['Mr', 'Miss', 'Ms', 'Mrs', 'Dr'];
                         foreach($prefix as $value) :?>
-                        <option value="<?php echo $value; ?>">
+                        <?php  $selected = $registrationArray['prefix'] == $value ? 'selected' : ''?>
+                        <option value="<?php echo $value; ?>" <?php echo $selected; ?>>
                             <?php echo $value; ?>
                         </option>
                     <?php endforeach ?>
                 </select> <br><br>
             
             <label for="registration[firstName]"> First Name </label>
-            <input type="text" name="registration[firstName]" required/>
+            <input type="text" name="registration[firstName]" 
+                value="<?php echo $registrationArray['firstName']; ?>" required/>
             <span> 
                 <?php if(isset($_POST['registration']['firstName']))
                         echo validation('onlyText', $_POST['registration']['firstName']) 
@@ -28,7 +31,7 @@
             </span><br><br>
             
             <label for="registration[lastName]"> Last Name </label>
-            <input type="text" name="registration[lastName]" required/>
+            <input type="text" name="registration[lastName]" value="<?php echo $registrationArray['lastName'] ?>"; required/>
             <span> 
                 <?php if(isset($_POST['registration']['lastName']))
                         echo validation('onlyText', $_POST['registration']['lastName']) 
@@ -37,7 +40,7 @@
             </span><br><br>
             
             <label for="registration[mobile]">Mobile Number</label>
-            <input type="text" name="registration[mobile]" required/>
+            <input type="text" name="registration[mobile]" value="<?php echo $registrationArray['mobile'];?>" required/>
             <span>
                 <?php if(isset($_POST['registration']['mobile'])) 
                         echo validation('mobile', $_POST['registration']['mobile']) 
@@ -46,7 +49,7 @@
             </span><br><br>
 
             <label for="registration[email]">Email Id</label>
-            <input type="email" name="registration[email]" required/>
+            <input type="email" name="registration[email]" value="<?php echo $registrationArray['email'];?>" required/>
             <span>
                 <?php if(isset($_POST['registration']['email'])) 
                         echo validation('email', $_POST['registration']['email']) 
@@ -55,7 +58,7 @@
             </span><br><br>
 
             <label for="registration[userPassword]">Password</label>
-            <input type="password" name="registration[userPassword]" required/>
+            <input type="password" name="registration[userPassword]" value="<?php echo $registrationArray['userPassword'];?>" required/>
             <span> 
                 <?php if(isset($_POST['registration']['userPassword'])) 
                         echo validation('password', $_POST['registration']['userPassword']) 
@@ -64,7 +67,7 @@
             </span><br><br>
 
             <label for="confirmPassword">Confirm Password</label>
-            <input type="password" name="confirmPassword" required/>
+            <input type="password" name="confirmPassword" value="<?php echo $registrationArray['userPassword'];?>" required/>
             <span>
                 <?php if(isset($_POST['confirmPassword'])) 
                         echo checkPassword($_POST['registration']['userPassword'], $_POST['confirmPassword']) 
@@ -73,7 +76,7 @@
             </span><br><br>
 
             <label for="registration[information]"> Information </label>
-            <textarea name="registration[information]" required></textarea>
+            <textarea name="registration[information]" required><?php echo $registrationArray['information']?></textarea>
             <br><br>
         
             <input type="submit" name="btnUpdate" value="Update" />
