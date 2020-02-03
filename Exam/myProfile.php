d3cc481365b0900d6c4ca3b5579e7458d622f1c4
Dhara Parekh <parekhdhara94@gmail.com>
2020-02-03 19:21:38 +0530

    exam

---
+++ Exam/myProfile.php
@@ -0,0 +1,35 @@
+<?php 
+    session_start();
+    include_once 'connection.php';
+
+    if(isset($_SESSION['userName'])) {
+        if(isset($_POST['btnUpdate'])) {
+            updateRowData('userTable', $_SESSION['userId'], $_POST['registration']);
+        }
+    }
+    
+    function updateRowData($tableName, $customer_id, $arrayData) { //
+        global $connectionObject;
+        $updateData = '';
+        foreach($arrayData as $key => $value) {
+            $updateData .= ", $key = '$value'";
+            $updateData = ltrim($updateData, ', ');  
+        }
+        $updateQuery = "UPDATE $tableName SET $updateData WHERE customer_id=".$customer_id;
+        return mysqli_affected_rows($connectionObject);
+    }
+
+    function fetchData($tableName) {
+        global $connectionObject;
+        $query = "SELECT prefix, firstName, lastName, mobile, email, userPassword, information FROM $tableName WHERE userId =".$_SESSION['userId'];
+
+        if($result = mysqli_query($connectionObject, $query)) {
+            if(mysqli_num_rows($result) > 0) { 
+                foreach($result as $row => $column) {
+                    return $column;
+                }
+            }
+        } 
+    }
+
+?>
