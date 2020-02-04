0f2e7dcb3be870a8cfc12a9ec10184e77f225547
Dhara Parekh <parekhdhara94@gmail.com>
2020-02-04 18:12:51 +0530

    exam

--- Exam/addCategory.php
+++ Exam/addCategory.php
@@ -3,8 +3,12 @@
     include_once 'connection.php';
     if(isset($_SESSION['userName'])) {
         if(isset($_POST['btnAddNewCategory'])) {
-            insertFunction('category', $_POST['category']);
-            header("location: categoryDesign.php");
+            if(getImageData()) {
+                $_POST['category']['image'] =  $_FILES['image']['name'];
+                 insertFunction('category', $_POST['category']);
+                 header("location: categoryDesign.php");
+            }
+            
         }
         
     } else {
@@ -38,4 +42,30 @@
         $insertedResult = mysqli_query($connectionObject, $insertedQuery);
     }
 
+    function getImageData() {
+        if(!empty($_FILES['image'])) {
+            $name = $_FILES['image']['name'];
+            $size = $_FILES['image']['size'];
+            $type = $_FILES['image']['type'];
+            $tmp_name = $_FILES['image']['tmp_name'];
+            $uploadPath = 'uploads/';
+            $extension = strtolower(substr($name,strpos($name,'.')+1));
+            if(($extension === 'jpeg' || $extension === 'png' || $extension === 'jpg') && ($type === 'image/png' || $type === 'image/jpeg' || $type === 'image/jpg')) {
+                    if($size < 3526840) {
+                        if(move_uploaded_file($tmp_name,$uploadPath.$name)) {
+                            echo "File Uploaded";
+                            return true;
+                        } else {
+                            echo "Something want wrong";
+                        } 
+                    } else {
+                        echo "Please select file upto 2 Mb";
+                    }
+            } else {
+                echo "Please select only image file";
+            }
+        } else {
+            echo "Please Select the file";
+        }
+    }
 ?>
