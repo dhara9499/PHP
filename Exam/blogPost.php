8ac119db0c04983439c701403e4763e5f5ac909e
Dhara Parekh <parekhdhara94@gmail.com>
2020-02-04 18:10:40 +0530

    exam

--- Exam/blogPost.php
+++ Exam/blogPost.php
@@ -11,20 +11,26 @@
                     <body>
                         <div>
                             <table border="1">';
-            if($result = mysqli_query($connectionObject, $selectQuery)) {
-                if(mysqli_num_rows($result) > 0) { 
-                    foreach($result as $row => $column) {   
-                        foreach($column as $value) {
-                            echo '<td>'.$value.'</td>'; 
-                        } 
-                        echo "<td><a href='#'>Update</a></td>";
-                        echo "<td><a href='#'>Delete</a></td>";
-                        echo "</tr>";
-                    }
-                
+            echo '<tr>
+                    <th>Post Id</th>
+                    
+                    <th>Title</th>
+                    <th>Published At</th>
+                    <th colspan="2">Actions</th>  
+                  </tr>';
+                  if($result = mysqli_query($connectionObject, $selectQuery)) {
+                    if(mysqli_num_rows($result) > 0) { 
+                       while($column = mysqli_fetch_assoc($result)) {   
+                            echo '<td>'.$column['postId'].'</td>';
+                            echo '<td>'.$column['title'].'</td>';
+                            echo '<td>'.$column['publishedAt'].'</td>';   echo "<td><a href='updateBlogPostDesign.php?update=".$column['postId']."'>Update</a></td>";
+                            echo "<td><a href='?delete=".$column['postId']. "'>Delete</a></td>";
+                            echo "</tr>";
+                        }
+                    
+                    } 
+                    echo '</table></div></body></html>';   
                 } 
-                echo '</table></div></body></html>';   
-            } 
         }
         if(isset($_POST['btnLogout'])) {
             session_destroy();
@@ -40,8 +46,25 @@
         }
 
         if(isset($_POST['btnMyProfile'])) {
-            header("location: profile.php");
+            header("location: myProfileDesign.php");
         }
+
+        function deleteRowData($postId) { //delete function
+            global $connectionObject;
+            $deleteQuery = "DELETE FROM blogpost where postId = ".$postId;
+            $result_delete = mysqli_query($connectionObject, $deleteQuery);
+            
+            if ($result_delete) {
+                echo '<script>alert(\'Row deleted successfully\');</script>';
+            } else {
+                echo '<script>alert(\'Row deleted successfully\');</script>';
+            } 
+        }
+
+        if(isset($_REQUEST['delete'])) { //execution of delete function
+            deleteRowData($_REQUEST['delete']);
+        }
+
        
     } else {
         header("location: loginDesign.php");
